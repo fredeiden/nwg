b7c989b7f0a59876c2c5f8724a951afbfcca286e
fredeiden <fredeiden@yahoo.com>
2020-10-13 21:17:27 -0700

    Removed debugging statement

--- modules/custom/nwg_util/src/Plugin/Menu/LocalTask/PortfolioTab.php
+++ modules/custom/nwg_util/src/Plugin/Menu/LocalTask/PortfolioTab.php
@@ -11,7 +11,6 @@ class PortfolioTab extends LocalTaskDefault {
   * {@inheritdoc}
   */
   public function getRouteParameters(RouteMatchInterface $route_match) {
-    dsm($route_match->getRouteObject());
     $user_id = $route_match->getParameter('user');
     return [
       'user' => isset($user_id) ? \Drupal::currentUser()->id() : $user_id,
